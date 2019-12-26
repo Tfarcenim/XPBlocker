cd59813008a15116e149dbf3ab44ea690538e495
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-12-25 22:13:27 -0500

    more cleanup

--- src/main/java/com/mrtrollnugnug/xpBlocker/EventManager.java
+++ src/main/java/com/mrtrollnugnug/xpBlocker/EventManager.java
@@ -10,7 +10,7 @@ import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class EventManager {
-	
+
 	@SubscribeEvent
 	public void onEntityJoinWorld(EntityJoinWorldEvent event){
 		if (event.getEntity() instanceof ExperienceOrbEntity){
@@ -23,14 +23,6 @@ public class EventManager {
 	@SubscribeEvent
 	public void onPlayerUseBlock(PlayerInteractEvent.RightClickBlock event){
 			if (event.getItemStack().getItem() == Items.EXPERIENCE_BOTTLE){
-				int xp = 5;
-				for (int i = 0; i < xp; i ++){
-					if (!event.getWorld().isRemote){
-						ExperienceOrbEntity orb = new ExperienceOrbEntity(event.getWorld(), event.getPos().getX()+0.5, event.getPos().getY()+0.5, event.getPos().getZ()+0.5, 2);
-						orb.getPersistentData().putBoolean("XPBLOCKER_protected", true);
-						event.getWorld().addEntity(orb);
-					}
-				}
 				event.setCanceled(true);
 			}
 	}
@@ -38,14 +30,6 @@ public class EventManager {
 	@SubscribeEvent
 	public void onPlayerUse(PlayerInteractEvent.RightClickItem event){
 			if (event.getItemStack().getItem() == Items.EXPERIENCE_BOTTLE){
-				int xp = 5;
-				for (int i = 0; i < xp; i ++){
-					if (!event.getWorld().isRemote){
-						ExperienceOrbEntity orb = new ExperienceOrbEntity(event.getWorld(), event.getPos().getX()+0.5, event.getPos().getY()+0.5, event.getPos().getZ()+0.5, 2);
-						orb.getPersistentData().putBoolean("XPBLOCKER_protected", true);
-						event.getWorld().addEntity(orb);
-					}
-				}
 				event.setCanceled(true);
 			}
 	}
@@ -53,15 +37,6 @@ public class EventManager {
 	@SubscribeEvent
 	public void onBlockBreak(BreakEvent event){
 		if (event.getExpToDrop() > 0){
-			int xp = event.getExpToDrop();
-			for (int i = 0; i < xp; i ++){
-				World world = (World)event.getWorld();
-				if (!world.isRemote){
-					ExperienceOrbEntity orb = new ExperienceOrbEntity(world, event.getPos().getX()+0.5, event.getPos().getY()+0.5, event.getPos().getZ()+0.5, 2);
-					orb.getPersistentData().putBoolean("XPBLOCKER_protected", true);
-					event.getWorld().addEntity(orb);
-				}
-			}
 			event.setExpToDrop(0);
 		}
 	}
@@ -69,6 +44,6 @@ public class EventManager {
 	@SubscribeEvent
 	public void onSmelt(PlayerEvent.ItemSmeltedEvent event){
 			int xp = (int) (event.getSmelting().getItem().getSmeltingExperience(event.getSmelting())*event.getSmelting().getMaxStackSize());
-			event.getPlayer().giveExperiencePoints(xp);
+			if (XPBlocker.ServerConfig.smelt_xp.get())event.getPlayer().giveExperiencePoints(xp);
 	}
 }
--- src/main/java/com/mrtrollnugnug/xpBlocker/XPBlocker.java
+++ src/main/java/com/mrtrollnugnug/xpBlocker/XPBlocker.java
@@ -1,21 +1,44 @@
 package com.mrtrollnugnug.xpBlocker;
 
+import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod(XPBlocker.MODID)
-public class XPBlocker
-{
-    public static final String MODID = "xpblocker";
+public class XPBlocker {
+  public static final String MODID = "xpblocker";
 
-    public XPBlocker(){
-			FMLJavaModLoadingContext.get().getModEventBus().addListener(this::preInit);
-		}
+  public XPBlocker() {
+    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::preInit);
+  }
 
-    private void preInit(FMLCommonSetupEvent event){
-    	MinecraftForge.EVENT_BUS.register(new EventManager());
-    	MinecraftForge.EVENT_BUS.register(new ConfigManager());
+  private void preInit(FMLCommonSetupEvent event) {
+    MinecraftForge.EVENT_BUS.register(new EventManager());
+  }
+
+  public static final ServerConfig SERVER;
+  public static final ForgeConfigSpec COMMON_SPEC;
+
+  static {
+    final Pair<ServerConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ServerConfig::new);
+    COMMON_SPEC = specPair.getRight();
+    SERVER = specPair.getLeft();
+  }
+
+  public static class ServerConfig {
+
+    public static ForgeConfigSpec.BooleanValue smelt_xp;
+
+    ServerConfig(ForgeConfigSpec.Builder builder) {
+
+      builder.push("general");
+      smelt_xp = builder
+              .comment("Toggles whether you get XP from smelting.")
+              .define("smelt_xp", false);
+      builder.pop();
     }
+  }
 }
